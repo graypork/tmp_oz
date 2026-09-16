f48da238c242058a9ba0a5949dc3074b188336ba
graypork <87932375+graypork@users.noreply.github.com>
2026-09-16 21:46:08 +0900

    Change title from '틀린 그림 찾기' to '다른 그림 찾기'

--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -7,7 +7,7 @@ export function StartScreen({ onStart }: Props) {
     <main className="start-screen">
       <section className="start-card">
         <div className="start-badge">부모참여수업</div>
-        <h1>틀린 그림 찾기</h1>
+        <h1>다른 그림 찾기</h1>
         <p>각색된 명화를 터치해서 원본 명화와 다른 곳을 찾아보세요.</p>
         <div className="start-rule">
           <span>각색된 명화만 터치</span>
